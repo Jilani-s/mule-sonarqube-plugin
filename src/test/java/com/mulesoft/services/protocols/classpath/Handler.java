2118bf9f8ebb45f976880fcedb38ee0552da0810
Jilani-s <106386175+Jilani-s@users.noreply.github.com>
2022-06-17 11:12:26 +0530

    Update Handler.java

--- src/test/java/com/mulesoft/services/protocols/classpath/Handler.java
+++ src/test/java/com/mulesoft/services/protocols/classpath/Handler.java
@@ -7,20 +7,5 @@ import java.net.URLStreamHandler;
 
 public class Handler extends URLStreamHandler {
 
-	private final ClassLoader classLoader;
-
-	public Handler() {
-		this.classLoader = getClass().getClassLoader();
-	}
-
-	public Handler(ClassLoader classLoader) {
-		this.classLoader = classLoader;
-	}
-
-	@Override
-	protected URLConnection openConnection(URL u) throws IOException {
-		final URL resourceUrl = classLoader.getResource(u.getPath());
-		return resourceUrl.openConnection();
-	}
 
 }
